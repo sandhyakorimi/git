1130742af2162bc89b18b03855b97688b17d8f9e
SANDHYA <24P31F00D4@acet.ac.in>
2025-05-27 10:08:09 +0530

    ConsonantOrVowel

---
+++ ConsonantOrVowel.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ConsonantOrVowel {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character : ");
+        char ch = sc.next().toLowerCase().charAt(0);
+        if(!Character.isLetter(ch)){
+            System.out.println("not a character");
+        }
+        else if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+            System.out.println("the character "+ch+" is vowel");
+        }
+        else{
+            System.out.println("the character "+ch+ " is Consonant");
+        }
+        sc.close();
+    }
+}
