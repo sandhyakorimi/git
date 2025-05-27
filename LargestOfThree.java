5dbce1d3ecac2b28708928595de8d6a1f5c754f9
SANDHYA <24P31F00D4@acet.ac.in>
2025-05-27 10:37:53 +0530

    LargestOfThree

---
+++ LargestOfThree.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class LargestOfThree {
+   public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.println("Enter third number: ");
+        int c = sc.nextInt();
+        if(a==b&&a==c){
+            System.out.println("all three are same");
+        }
+        else if(a>=b&&a>=c){
+            System.out.println("first number a is big : "+a);
+        }
+        else if(b>=a&&b>=c){
+            System.out.println("second number b is big : "+b);
+        }
+        else{
+            System.out.println("third number c is big : "+c);
+        }
+        sc.close();
+   } 
+}
