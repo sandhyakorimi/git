a70627eb97e79253e43128d151457d54480e9d4e
SANDHYA <24P31F00D4@acet.ac.in>
2025-05-27 22:15:08 +0530

    PositiveOrNegative

---
+++ PositiveOrNegative.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PositiveOrNegative {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = sc. nextInt();
+           if(num==0){
+            System.out.println(num+" is zero");
+           }
+           else if(num>0){
+                System.out.println(num+" is Positive Number");
+            }
+            else {
+                System.out.println(num+" is Negative Number");
+            }
+         sc.close();
+    }
+   
+}
