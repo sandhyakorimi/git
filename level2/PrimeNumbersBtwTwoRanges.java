8f20f0e9fc9ae6fb71db517bea1366280e550416
SANDHYA <24P31F00D4@acet.ac.in>
2025-05-29 14:44:24 +0530

    Prime Numbers Between Two Ranges

---
+++ level2/PrimeNumbersBtwTwoRanges.java
@@ -0,0 +1,31 @@
+package level2;
+
+import java.util.Scanner;
+
+public class PrimeNumbersBtwTwoRanges {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int start = sc. nextInt();
+        int end = sc.nextInt();
+        System.out.println("The prime numbers between "+start+" and "+end+" are :");
+        for(int i=start;i<end;i++){
+            if(isPrime(i)){
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        }
+         sc.close();
+    }
+    public static boolean isPrime(int num){
+        if(num<=1){
+            return false;
+        }
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
