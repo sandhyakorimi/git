4a0bdeb9914549f9f7ecfb907cc89fcfacf805d4
SANDHYA <24P31F00D4@acet.ac.in>
2025-05-31 22:04:59 +0530

    butterfly pattern

--- Patterns/butterfly1.java
+++ Patterns/butterfly1.java
@@ -2,29 +2,36 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class diamond {
+public class butterfly1 {
       public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int n = sc. nextInt();
         for(int i=1; i<=n; i++){
-            for(int spaces=1; spaces<=n-i; spaces++){
-                System.out.print(" "); 
-            }
             for(int j=1; j<=i; j++){
                 System.out.print("* ");
             }
-            System.out.println();
-        }
-        for(int i=n-1; i>=1; i--){
-            for(int spaces=1; spaces<=n-i; spaces++){
+            for(int spaces=1;spaces<=4*(n-i);spaces++){
                 System.out.print(" ");
             }
-            for(int j=1; j<=i; j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
+
+       for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-i; j++){
+                System.out.print("* ");
+            }
+            for(int spaces=1; spaces<=4*i; spaces++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=n-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+       }        
 
         sc.close();
     }
