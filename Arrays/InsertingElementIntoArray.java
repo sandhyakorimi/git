2b391c58f6cad778b65611c23b895039c1f66271
SANDHYA <24P31F00D4@acet.ac.in>
2025-06-01 21:32:16 +0530

    Inserting Element into Array

---
+++ Arrays/InsertingElementIntoArray.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class InsertingElementIntoArray {
+        public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter elements to array : ");
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter new element to be inserted : ");
+        int newElement = sc.nextInt();
+
+        int newArr[] = new int[n+1];
+        for(int i=0; i<n; i++){
+            newArr[i] = arr[i];
+        }
+        newArr[n] = newElement;
+
+        System.out.print("New array after inserting "+newElement+" : ");
+        for(int num:newArr){
+            System.out.print(num+" ");
+        }
+        sc.close();
+    }
+
+}
