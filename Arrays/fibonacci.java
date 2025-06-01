0a54dddcb4fd243eedf5f8fede8f70c4840bbeef
SANDHYA <24P31F00D4@acet.ac.in>
2025-06-01 15:10:10 +0530

    fibonacci using arrays

---
+++ Arrays/fibonacci.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class fibonacci {
+      public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a how many numbers you want to print : ");
+        int n = sc. nextInt();
+        if(n<=0){
+            System.out.println("enter a positive number");
+        }
+        long[] fib = new long[n];
+        fib[0]=0;
+        if(n>1){
+            fib[1]=1;
+            for(int i=2; i<n; i++){
+                fib[i] = fib[i-1]+fib[i-2];
+            }
+            for(int i=0; i<n; i++){
+                System.out.print(fib[i]+" ");
+            }
+        }
+        
+      sc.close();
+    }
+}
