65c8724bbf477378ba60cc2aa653fb5939bbf861
SANDHYA <24P31F00D4@acet.ac.in>
2025-06-01 14:47:10 +0530

    big factorial

---
+++ Arrays/BigFactorial.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial {
+      public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc. nextInt();
+        BigInteger fact = BigInteger.ONE;
+        for(int i=2; i<=n; i++){
+          fact = fact.multiply(BigInteger.valueOf(n));
+        }
+        System.out.println("The factorial of "+n+" is : "+fact);
+        sc.close();
+    }  
+}
