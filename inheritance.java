b13e06d60a85252b48a6baa8b0d91886210724a2
SANDHYA <24P31F00D4@acet.ac.in>
2025-08-08 23:40:37 +0530

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,16 @@
+class Animal {
+    // Animal class with no methods
+}
+
+class Dog extends Animal {
+    // Dog class inherits from Animal
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        Dog d = new Dog(); // Creating a Dog object
+
+        System.out.println(d instanceof Dog);    // true
+        System.out.println(d instanceof Animal); // true (Dog is a subclass of Animal)
+    }
+}
