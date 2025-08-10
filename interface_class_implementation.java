ca5201828107bbc6981617947462bf6e9f56e091
SANDHYA <24P31F00D4@acet.ac.in>
2025-08-10 15:32:24 +0530

    interface-class

---
+++ interface_class_implementation.java
@@ -0,0 +1,21 @@
+public class interface_class_implementation {
+    public static void main(String [] args){
+        c obj1 = new c();
+        obj1.sound();
+        obj1.eat();
+    }
+}
+interface A{
+    void sound();
+}
+interface B extends A{
+     void eat();
+}
+class c implements B{
+    public void sound(){
+        System.out.println("Barking");
+    }
+    public void eat(){
+        System.out.println("Eating");
+    }
+}
