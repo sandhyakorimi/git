b5721a0690207a80e095fadf765320d04dc0e2ee
SANDHYA <24P31F00D4@acet.ac.in>
2025-08-23 20:46:38 +0530

    polymorphism

--- polymorphism.java
+++ polymorphism.java
@@ -1,23 +1,54 @@
+// class Calculator {
+//     int add(int a, int b) {
+//         return a + b;
+//     }
 
-// Compile-time polymorphism example (Method Overloading)
-class Calculator {
-    // Adds two integers
-    int add(int a, int b) {
-        return a + b;
-    }
+//     int add(int a, int b, int c) {
+//         return a + b + c;
+//     }
 
-    // Adds three integers
-    int add(int a, int b, int c) {
-        return a + b + c;
-    }
+//     double add(double a, double b) {
+//         return a + b;
+//     }
+// }
 
-    // Adds two double numbers
-    double add(double a, double b) {
-        return a + b;
-    }
-}
+// class Animal {
+//     void sound() {
+//         System.out.println("Animal makes a sound");
+//     }
+// }
 
-// Runtime polymorphism example (Method Overriding)
+// class Dog extends Animal {
+//     @Override
+//     void sound() {
+//         System.out.println("Dog barks");
+//     }
+// }
+
+// class Cat extends Animal {
+//     @Override
+//     void sound() {
+//         System.out.println("Cat meows");
+//     }
+// }
+
+// public class polymorphism {
+//     public static void main(String[] args) {
+//         Calculator calc = new Calculator();
+//         System.out.println("Add 2 numbers: " + calc.add(5, 10));
+//         System.out.println("Add 3 numbers: " + calc.add(5, 10, 15));
+//         System.out.println("Add 2 doubles: " + calc.add(5.5, 3.2));
+
+//         Animal a;
+//         a = new Dog();
+//         a.sound(); 
+
+//         a = new Cat();
+//         a.sound();  
+//     }
+// }
+
+
 class Animal {
     void sound() {
         System.out.println("Animal makes a sound");
@@ -25,14 +56,12 @@ class Animal {
 }
 
 class Dog extends Animal {
-    @Override
     void sound() {
         System.out.println("Dog barks");
     }
 }
 
 class Cat extends Animal {
-    @Override
     void sound() {
         System.out.println("Cat meows");
     }
@@ -40,18 +69,12 @@ class Cat extends Animal {
 
 public class polymorphism {
     public static void main(String[] args) {
-        // Compile-time polymorphism
-        Calculator calc = new Calculator();
-        System.out.println("Add 2 numbers: " + calc.add(5, 10));
-        System.out.println("Add 3 numbers: " + calc.add(5, 10, 15));
-        System.out.println("Add 2 doubles: " + calc.add(5.5, 3.2));
+        Animal a;  // reference of parent class
 
-        // Runtime polymorphism
-        Animal a;
-        a = new Dog();
-        a.sound();  // Calls Dog's sound()
+        a = new Dog();  
+        a.sound();   // Output: Dog barks
 
-        a = new Cat();
-        a.sound();  // Calls Cat's sound()
+        a = new Cat();  
+        a.sound();   // Output: Cat meows
     }
 }
