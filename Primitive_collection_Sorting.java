a492936f020e6b69f91e15260a86359e502abb9f
SANDHYA <24P31F00D4@acet.ac.in>
2025-08-29 22:28:58 +0530

    primitive collection Sorting

---
+++ Primitive_collection_Sorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+public class Primitive_collection_Sorting {
+
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4};
+        Arrays.sort(array);
+
+        for(int num:array){
+            System.out.print(num+" ");
+        }
+    }
+}
--- polymorphism.java
+++ polymorphism.java
@@ -82,4 +82,24 @@ public class polymorphism {
 
 
 
+// Annaya nitho matladali Prasad vishyam ani adigithe vadu nak. Samabandhledhu nak teleedhu edhokati antadu but nuv 
+// ledhu annaya matladali nuv vadu msg chesthe em cheppatledhugaa Inka dinni laaga kunda oka conclusion ki ravali
+//  maku Inka aa alochana lekunda prepare avvali nuvvu Prasad job techukunte ma gurinchi intlo cheppi maku nuv help
+//   cheyyali arrange marriage lagaaa nen love chesinatlu kakundaa and daddy nuv chepthe vintaru alochistharu manchi
+//    abbai mana caste aa nannu vachi adigadu mi chelli ante istam pelli cheskunta ani asthi kuda parledhu oka 2,3 crs 
+//    untundhi lands  family kuda amma nanna abbaye inkevaru leru valladhi Ongole dhaggera  valla father farmer a
+//     Vadiki job kuda ochindhi baga chadhuvthadle mana sandhya lagaa ani ivanni Cheppu nuv matladu annaya daddy ni
+//      maata vintaru nuv maatladtha maku help chestha Ani nuv chepthe nen vadu happy ga prepare avtham job kosam
+//       annaya ninnu eppudu edhi adagaledhu e okkasariki nakosam idhi okkati chey raaa nen inkevarni uhinchukolenu 
+//       vadni tappa nak Inka a matches ranivvadhu edhokati cheppuu e okka vishyam lo help cheyyra annayaa ani gettiga 
+//       pattukoni edusthu adugu
+
+
+
+
+
+
+
+
+
 
