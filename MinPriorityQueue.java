934c41a55b85d72b22e318f1a8ffdca7ba370f32
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-03 21:24:48 +0530

    MinPriorityQueue

--- MinPriorityQueue.java
+++ MinPriorityQueue.java
@@ -7,5 +7,10 @@ public class MinPriorityQueue {
         pq.add(45);
         pq.add(12);
         System.out.println("MinPriorityQueue: "+pq);
+        pq.forEach((Integer val)->System.out.println(val));
+        while (!pq.isEmpty()){
+            int val = pq.poll();
+            System.out.println("remove from top : "+val);
+        } 
     }
 }
