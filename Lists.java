c86f4e7b21116342c0988cd87d823eb85ab414b9
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-04 23:18:53 +0530

    Lists added some methods

--- Lists.java
+++ Lists.java
@@ -6,5 +6,15 @@ public class Lists {
         list1.add(24);
         list1.add(12);
         list1.forEach((Integer n)-> System.out.println("Element : " +n ));
+        
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(2);
+        list2.add(30);
+        list2.add(10);
+        list2.forEach((Integer n)-> System.out.println("Element : " +n));
+
+        list1.addAll(1,list2);
+            System.out.println("list 1 after adding list2");
+        list1.forEach((Integer n)->System.out.println("Element : " +n));
     }
 }
