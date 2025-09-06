e75b523df82b5df9b390f20ad2058308c7f05790
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-06 20:34:57 +0530

    Stack

---
+++ StackEx.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StackEx {
+    public static void main(String args[]){
+        Stack<Integer> stack = new Stack<>();
+        stack.push(34);
+        stack.push(12);
+        stack.forEach((Integer n)->System.out.println(n));
+        System.out.println("Pop is : "+stack.pop());
+    }
+}
