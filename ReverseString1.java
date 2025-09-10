119d77ff09e3f3cd7c18cb1305aff78073fe3608
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-10 20:44:44 +0530

    reverseString

--- ReverseString1.java
+++ ReverseString1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ReverseString1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a string to reverse: ");
         String str = sc.nextLine();
 
         String rev = "";
