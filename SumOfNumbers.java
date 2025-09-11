9fcd31fda4a94772f39fe0dde0bbb6a842d3404a
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-11 21:06:42 +0530

    sum of numbers

---
+++ SumOfNumbers.java
@@ -0,0 +1,12 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        int n = 10;  // you can change this value
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of numbers from 1 to " + n + " is: " + sum);
+    }
+}
