c552f1d78c91e6901d2233992c4f36177bb58daf
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-12 20:42:03 +0530

    Matrix Multiplication

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -3,7 +3,7 @@ public class MatrixMultiplication {
         // First matrix (2x3)
         int[][] A = {
             {1, 2, 3},
-            {4, 5, 6}
+            {4, 5, 9}
         };
 
         // Second matrix (3x2)
