6fafbeedb0b1735bd8794f681d4cd50d321542e2
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-13 21:55:14 +0530

    Fibonacci

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+class Fibonacci{
+    public static void main(String[] args) {
+        int n = 24;  // number of terms
+        int first = 0, second = 1;
+
+        System.out.print("Fibonacci Series as follows: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
