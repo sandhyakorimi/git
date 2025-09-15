5adeda3794ee694c0572d1b1cfe743e91ff91fb7
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-15 21:02:50 +0530

    LinkedHashMap Access

--- LinkedHashAccessEx.java
+++ LinkedHashAccessEx.java
@@ -5,6 +5,7 @@ public class LinkedHashAccessEx {
         acc.put(1,"A");
         acc.put(2, "B");
         acc.put(3,"D");
+        acc.put(4,"E");
         acc.get(2);
         acc.forEach((Integer key, String val)->System.out.println(key +" : "+ val));
     }
