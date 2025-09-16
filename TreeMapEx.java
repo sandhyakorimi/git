b7bdfe5e01535eaf601a6e1416d87cb9b9974a9e
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-16 12:27:51 +0530

    Tree Map

---
+++ TreeMapEx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TreeMapEx {
+    public static void main(String args[]){
+        //Using comporator
+        System.out.println("Using comporator : ");
+        Map<Integer, String> map1 = new TreeMap<>((Integer val1, Integer val2)->val2-val1);
+        map1.put(4,"D");
+        map1.put(1,"A");
+        map1.put(9,"I");
+        map1.put(2,"B");
+        map1.forEach((Integer n , String str) ->System.out.println(n+" : "+str));
+    }
+}
