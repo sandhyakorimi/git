60dcfa1f7ec5cfeee754cf1535e492a4e3b7a189
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-17 18:37:06 +0530

    ThreadSafeSet ex

---
+++ ThreadSafeSetEx.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+public class ThreadSafeSetEx {
+    public static void main(String[] args) {
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap<>();
+        Set<Integer> threadSafeSet = concurrentHashMap.newKeySet();
+        threadSafeSet.add(3);
+        threadSafeSet.add(8);
+
+        Iterator<Integer> iterator = threadSafeSet.iterator();
+        while (iterator.hasNext()) {
+            int val = iterator.next();
+            if(val==8){
+                threadSafeSet.add(9);         // due to thread safe it adds and runs another thread
+            }
+        }
+        threadSafeSet.forEach((Integer n)->System.out.println(n));
+    }
+}
