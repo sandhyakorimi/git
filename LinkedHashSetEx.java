3f8a0b56f18494badfc11e05f28c5c15884b64c1
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-17 18:57:53 +0530

    Linked Hash Set ex

---
+++ LinkedHashSetEx.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class LinkedHashSetEx {
+    public static void main(String[] args) {
+        Set<Integer> set1 = new LinkedHashSet<>();
+        set1.add(4);
+        set1.add(5);
+        set1.add(6);
+        set1.add(90);
+        set1.add(9);
+        Iterator<Integer> iterable = set1.iterator();
+        while (iterable.hasNext()) {
+            int val = iterable.next();
+            System.out.println(val);
+        }
+    }
+}
