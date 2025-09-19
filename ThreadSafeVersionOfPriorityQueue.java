00a9685c262a82dd35550484b37b3dbd484682d7
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-19 18:41:08 +0530

    ThreadSafeVersion of Priority queue ex

--- ThreadSafeVersionOfPriorityQueue.java
+++ ThreadSafeVersionOfPriorityQueue.java
@@ -5,6 +5,7 @@ public class ThreadSafeVersionOfPriorityQueue {
         PriorityBlockingQueue<Integer> pbq = new PriorityBlockingQueue<>(10,(Integer a, Integer b)->b-a);
         pbq.add(10);
         pbq.add(23);
+        pbq.add(90);
         pbq.forEach((Integer n )->System.out.println(n));
         System.out.println("Peek Element : " + pbq.peek());
     }
