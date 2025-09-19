3ac9e4c640d011ce48b04df00dd2f57a9a38625c
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-19 18:43:21 +0530

    ThreadSafeVersion of List

--- ThreadSafeVersionOfList.java
+++ ThreadSafeVersionOfList.java
@@ -4,6 +4,7 @@ public class ThreadSafeVersionOfList {
     public static void main(String[] args) {
         List<Integer> list1 = new CopyOnWriteArrayList<>();
         list1.add(0,2);
+        list1.add(1,31);
         System.out.println(list1.get(0));
     }
 }   
