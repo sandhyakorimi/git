bc4882906d6f45790ed33cdfdbf30b5f81a6b8d2
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-19 18:42:18 +0530

    ThreadSafeVersion of Array Deque ex

--- ThreadSafeVersionofArrayDeque.java
+++ ThreadSafeVersionofArrayDeque.java
@@ -8,6 +8,6 @@ public class ThreadSafeVersionofArrayDeque {
         CLD.addFirst(12);
         CLD.forEach((Integer n)->System.out.println(n));
         int rem = CLD.removeFirst();
-        System.out.println("Element Removed is : "+rem);
+        System.out.println("Element that Removed is : "+rem);
     }
 }
