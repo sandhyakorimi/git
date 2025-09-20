44d96a2b5d38079a30fe7244d7588b5ca2d22eb1
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-20 10:50:59 +0530

    vector

--- vectorEx.java
+++ vectorEx.java
@@ -6,6 +6,8 @@ public class vectorEx{
         vec.add(23);
         vec.add(34);
         vec.add(90);
+        // stack.pop();
+        vec.add(91);
         vec.forEach((Integer n)->System.out.println("vector element at index " +vec.indexOf(n)+" is "+ n));
         vec.remove(1);
         System.out.println("Element at index 1 is : "+vec.get(1));
