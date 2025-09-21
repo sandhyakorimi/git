c86105458fd308f22581f62eae1be81327b3f913
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-21 11:00:36 +0530

    Tree Set ex

--- TreeSetEx.java
+++ TreeSetEx.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class TreeSetEx {
     public static void main(String[] args) {
         Set<Integer> set1 = new TreeSet<>();
-        set1.add(3);
+        set1.add(3 );
         set1.add(1);
         set1.add(9);
         set1.add(112);
@@ -10,7 +10,7 @@ public class TreeSetEx {
         Iterator<Integer> iterable = set1.iterator();
         while (iterable.hasNext()) {
             int val = iterable.next();
-            System.out.println(val);
+            System.out.println(val );
            
         }
     }
