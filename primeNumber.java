8ebf6567f9be0632b727a43a7d6dac66e3e2f009
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-24 19:49:02 +0530

    PrimeNumber

--- primeNumber.java
+++ primeNumber.java
@@ -25,6 +25,7 @@
 import java.util.Scanner;
 public class primeNumber{
     public static void main(String args[]){
+        System.out.println("Enter a Number : ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         boolean isPrime = true;
