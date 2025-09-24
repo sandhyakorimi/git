fe741bd51e7a301189590069709a874631139b99
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-24 19:27:30 +0530

    reverse String

--- reverseString.java
+++ reverseString.java
@@ -5,7 +5,7 @@ public class reverseString {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String rev = "";
-        for(int i = str.length()-1; i>=0; i++){
+        for(int i = str.length()-1; i>=0; i--){
             rev = rev+str.charAt(i);
         }
         System.out.println(" Reverse String is : "+rev);
