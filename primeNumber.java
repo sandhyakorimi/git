7a585ca04a3342b1523c700977ca109661e54363
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-24 19:48:08 +0530

    PrimeNumber

--- primeNumber.java
+++ primeNumber.java
@@ -1,19 +1,48 @@
+// import java.util.Scanner;
+// public class primeNumber{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         if(num==0||num==1){
+//             System.out.println("are not prime ");
+//         }
+//         for(int i=2; i<=Math.sqrt(num); i++){
+//             if(num%i==0){
+//                 System.out.println(num+" : is not Prime");
+//                 break;
+//             }
+//             else{
+//                 System.out.println((num+" : is Prime"));
+//             }
+//         }
+//     }
+// }
+
+
+
+
+
 import java.util.Scanner;
 public class primeNumber{
-    public static void main(String[] args) {
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        if(num==0||num==1){
-            System.out.println("are not prime ");
+        boolean isPrime = true;
+        if(num<=1){
+            isPrime = false;
         }
-        for(int i=2; i<=Math.sqrt(num); i++){
-            if(num%i==0){
-                System.out.println(num+" : is not Prime");
-                break;
-            }
-            else{
-                System.out.println((num+" : is Prime"));
+        else{
+            for(int i=2; i<= Math.sqrt(num);i++){
+                if(num%i==0){
+                    isPrime = false;
+                    break;
+                }
             }
         }
+        if(isPrime){
+            System.out.println(num + " : is prime");
+        }
+        else System.out.println(num+ " : is not prime");
+        sc.close();
     }
 }
