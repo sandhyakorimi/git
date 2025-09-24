134aafd442354e2f0b5151b4067c462088c99a2b
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-24 19:39:27 +0530

    PrimeNumber

--- primeNumber.java
+++ primeNumber.java
@@ -6,7 +6,7 @@ public class primeNumber{
         if(num==0||num==1){
             System.out.println("are not prime ");
         }
-        for(int i=2; i<num/2; i++){
+        for(int i=2; i<=Math.sqrt(num); i++){
             if(num%i==0){
                 System.out.println(num+" : is not Prime");
                 break;
