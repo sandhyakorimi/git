2aa64aff8482ead21adcc4bf830508f0e7e36644
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-25 22:10:20 +0530

    Armstrong.java

---
+++ level2/ArmStrong2.java
@@ -0,0 +1,21 @@
+package level2;
+import java.util.Scanner;
+public class ArmStrong2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int temp = num;
+        int digits = String.valueOf(num).length();
+        int arm = 0;
+        while(temp!=0){
+            int digit = num%10;
+            arm += Math.pow(digit, digits);
+            temp/=10;
+        }
+        if(num==arm){
+            System.out.println("ArmStrong Number");
+        }
+        else System.out.println("Not ArmStrong Number");
+    }
+}
