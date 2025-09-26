cda3110e7f3e0c3a0eca2fc2353524af6f2c4c22
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-26 14:28:21 +0530

    Palin_drome

--- level2/Palin_drome.java
+++ level2/Palin_drome.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Palin_drome {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number or String : ");
+        System.out.println(" Enter a number or String : ");
         String str = sc.nextLine();
         String rev = "";
         for(int i=str.length()-1 ; i>=0; i--){
