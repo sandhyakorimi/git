150eeb48def021dfd6533c747310f183008c7706
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-26 14:27:38 +0530

    Palin_drome

---
+++ level2/Palin_drome.java
@@ -0,0 +1,17 @@
+// package level2;
+import java.util.Scanner;
+public class Palin_drome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number or String : ");
+        String str = sc.nextLine();
+        String rev = "";
+        for(int i=str.length()-1 ; i>=0; i--){
+            rev += str.charAt(i);
+        }
+        if(rev.equals(str)){
+            System.out.println(str + " : is Palindrome ");
+        }
+        else System.out.println(str+ " : is not a palindrome");
+    }
+}
