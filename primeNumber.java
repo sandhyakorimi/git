dbfb9ffd703ee980d019c1b04bc69169430b081b
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-27 18:17:56 +0530

    prime Number

--- primeNumber.java
+++ primeNumber.java
@@ -43,7 +43,7 @@ public class primeNumber{
         if(isPrime){
             System.out.println(num + " : is prime. ");
         }
-        else System.out.println(num+ " : is not prime. ");
+        else System.out.println(num+ " : is not prime.  ");
         sc.close();
     }
 }
