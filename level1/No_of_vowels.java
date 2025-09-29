c3b5676f68f408602a374c14094bb0f7fc5aadcf
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-29 17:06:14 +0530

    No of vowels

---
+++ level1/No_of_vowels.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class No_of_vowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String : ");
+        String str = sc.nextLine().toLowerCase();
+        int n = str.length();
+        String vowels = " ";
+        int num =  0;
+        for(int i=0 ; i<n; i++){
+            char ch = str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                num += 1;
+                vowels += ch+" ";
+            } 
+        }
+        System.out.println("The String "+str+" contains "+num+ " vowels. "+ "Those are "+vowels);
+        sc.close();
+    }
+}
