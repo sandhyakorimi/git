fb3c664f0855d6a7c051d1e2ad1f50671ef41324
SANDHYA <24P31F00D4@acet.ac.in>
2025-09-30 18:21:43 +0530

    Sum of elements in array

---
+++ level1/Sum_of_elements_in_array.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Sum_of_elements_in_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+       
+        int sum = 0;
+        for(int i = 0 ; i<=n-1; i++){
+            arr[i]= sc.nextInt();
+            sum += arr[i];
+        }
+        System.out.println(" The sum of numbers in a array is : "+sum);
+        sc.close();
+    }
+}
