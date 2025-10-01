e32dee291d1d1d72e9c6d6cf2e8efe4442a313d4
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-01 11:21:20 +0530

    Fibonacci_using_recursion

--- level1/Fibonacci_using_recursion.java
+++ level1/Fibonacci_using_recursion.java
@@ -1,17 +1,45 @@
+// import java.util.Scanner;
+// public class Fibonacci_using_recursion{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a Number to find the value in fibonacci series : ");
+//         int n = sc.nextInt();
+//         int a = 0; 
+//         int b = 1; 
+//         int c = 0;
+//         for(int i=2; i<=n; i++){
+//             c = a+b;
+//             a = b;
+//             b = c;
+//         }
+//         System.out.println("The "+n+"th fibonocci number is : "+a);
+//     }
+// }
+
+
+
 import java.util.Scanner;
+
+
 public class Fibonacci_using_recursion{
-    public static void main(String[] args) {
+    public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a Number to find the value in fibonacci series : ");
+        System.out.println("Enter a number to find fibonacci value : ");
         int n = sc.nextInt();
-        int a = 0; 
-        int b = 1; 
-        int c = 0;
-        for(int i=2; i<=n; i++){
-            c = a+b;
-            a = b;
-            b = c;
+        System.out.println("Fibonacci series is : ");
+        for(int i=0; i<n; i++){
+            System.out.print(fibonacci(i)+" ");
         }
-        System.out.println("The "+n+"th fibonocci number is : "+a);
+    }
+    public static int fibonacci(int n){
+        if(n==0){
+            return 0;
+        }
+        else if(n==1) {
+            return 1;
+        }
+        else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
     }
 }
