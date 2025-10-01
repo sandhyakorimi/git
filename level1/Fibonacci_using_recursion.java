358e80f38a9fb87773b1954dc467144fdc06fdc2
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-01 10:57:57 +0530

    Fibonacci_using_recursion

---
+++ level1/Fibonacci_using_recursion.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Fibonacci_using_recursion{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number to find the value in fibonacci series : ");
+        int n = sc.nextInt();
+        int a = 0; 
+        int b=1; 
+        int c = 0;
+        for(int i=2; i<=n;i++){
+            c = a+b;
+            a=b;
+            b = c;
+        }
+        System.out.println("The "+n+"th fibonocci number is: "+a);
+    }
+}
