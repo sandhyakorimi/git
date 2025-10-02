58db1b153e893246d5e7b2fbcfd93c590c3f1ae7
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-02 16:28:33 +0530

    Remove duplicates

--- level1/Remove_duplicate.java
+++ level1/Remove_duplicate.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class Remove_duplicate {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(" Enter number of elements to add into an array : ");
+        System.out.println("Enter number of elements to add into an array : ");
         int n = sc.nextInt();
-        System.out.println(" Enter "+n+ " elements : ");
+        System.out.println("Enter "+n+ " elements : ");
         int arr[] = new int[n];
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
