5ae088efd2a898e84d3b27ad9290d49ad2c7f24e
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-03 14:20:26 +0530

    Anagram

---
+++ level1/Anagram.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Anagram {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the First String : ");
+        String str1= sc.nextLine().toLowerCase().replaceAll("\\s","" );
+        System.out.println("Enter the Second String : ");
+        String str2 =sc.nextLine().toLowerCase().replaceAll("\\s", "");
+        if(str1.length()!=str2.length()){
+            System.out.println("Not Anagram ");
+        }
+        else{
+            char[] arr1 = str1.toCharArray();
+            char[] arr2 = str2.toCharArray();
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            if(Arrays.equals(arr1,arr2)){
+                System.out.println("Anagram");
+            }
+            else{
+                System.out.println("Not Anagram");
+            }
+        }
+        sc.close();
+    }
+}
