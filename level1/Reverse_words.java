7820db8d9fc2313f8d17aa0faa4eeabee2d89e32
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-05 17:47:22 +0530

    reverese words

--- level1/Reverse_words.java
+++ level1/Reverse_words.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Reverse_words {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a sentence : ");
+        System.out.println(" Enter a sentence : ");
         String sentence = sc.nextLine();
         System.out.println();
         String[] words = sentence.split(" ");
