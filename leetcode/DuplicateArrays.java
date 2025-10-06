66067e3188b4719ddeabdbc36ee73adef208afe9
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-06 20:52:38 +0530

    duplicates arrays

--- leetcode/DuplicateArrays.java
+++ leetcode/DuplicateArrays.java
@@ -11,7 +11,7 @@ class Solution {
         return ans; 
     }
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); 
         int n = sc.nextInt();
         int[] nums = new int[n];
         for(int i=0; i<n; i++){
