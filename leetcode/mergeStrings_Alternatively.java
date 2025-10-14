916718e61ceda1f43de4c58808cf4780c0342ea9
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-14 19:42:19 +0530

    merge strings alternatively

--- leetcode/mergeStrings_Alternatively.java
+++ leetcode/mergeStrings_Alternatively.java
@@ -6,7 +6,7 @@ public class mergeStrings_Alternatively {
         while(i<word1.length() && j<word2.length()){
             res.append(word1.charAt(i++));
             res.append(word2.charAt(j++));
-        }
+        } 
         res.append(word1.substring(i));
         res.append(word2.substring(j));
         return res.toString(); 
