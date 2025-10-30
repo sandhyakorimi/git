d3b7884d9d2eef6b50fa31c06b244513104d6af9
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-30 18:51:54 +0530

    Kth largest

---
+++ leetcode/Kth_Largest.java
@@ -0,0 +1,25 @@
+public class Kth_Largest {
+
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for (int num : nums) {
+            minHeap.offer(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+    }
+
+    public int add(int val) {
+        minHeap.offer(val);
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+
+}
