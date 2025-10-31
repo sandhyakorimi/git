5707b87f0b489e5c3e84eb918cc3752919934518
SANDHYA <24P31F00D4@acet.ac.in>
2025-10-31 19:54:40 +0530

    ExcelSheet_column

---
+++ leetcode/ExcelSheet_column.java
@@ -0,0 +1,12 @@
+public class ExcelSheet_column {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder res = new StringBuilder();
+        while (columnNumber > 0) {
+            columnNumber--;
+            int offset = columnNumber % 26;
+            res.append((char) ('A' + offset));
+            columnNumber /= 26;
+        }
+        return res.reverse().toString();
+    }
+}
