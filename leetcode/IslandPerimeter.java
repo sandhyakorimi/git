090395eb5fac99e9e3625ccec35b23f08388de25
SANDHYA <24P31F00D4@acet.ac.in>
2025-11-02 20:12:10 +0530

    Island Perimeter

--- leetcode/IslandPerimeter.java
+++ leetcode/IslandPerimeter.java
@@ -8,7 +8,7 @@ public class IslandPerimeter {
                     res += (j + 1 >= n || grid[i][j + 1] == 0) ? 1 : 0;
                     res += (i - 1 < 0 || grid[i - 1][j] == 0) ? 1 : 0;
                     res += (j - 1 < 0 || grid[i][j - 1] == 0) ? 1 : 0;
-                }
+                }       
             }   
         }
         return res;     
