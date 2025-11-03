d7266ecd1a1ef949830b3caaf036c225afb25ab6
SANDHYA <24P31F00D4@acet.ac.in>
2025-11-03 19:57:22 +0530

    Kth largest

---
+++ leetcode/Kth_Largest1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Kth_Largest1 {
+    List<Integer> arr;
+    int K;
+    public KthLargest(int k, int[] nums) {
+        K = k;
+        arr = new ArrayList();
+        for (int i = 0; i < nums.length; i++) {
+            arr.add(nums[i]);
+        }
+    }
+
+    public int add(int val) {
+        arr.add(val);
+        Collections.sort(arr);
+        return arr.get(arr.size() - K);
+    }
+
+}
